dc976b70340d291756b198f7dbc94778714de136
NN Fathoni <naufalfathoni79@gmail.com>
2021-12-14 00:29:05 +0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="px-4 py-8 max-w-4xl mx-auto">
+    <div class="flex flex-col items-center justify-center space-y-8 sm:flex-row sm:space-y-0 sm:space-x-8">
+      <div class="flex-shrink-0">
+        <img class="w-64 sm:w-48" alt="Vue logo" src="./assets/logo.png" />
+      </div>
+      <div class="">
+        <HelloWorld msg="Hello Tailwind + Vue + Vite" />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold text-gray-900">{{ msg }}</h1>
+
+  <p class="text-gray-900">
+    Recommended IDE setup:
+    <a
+      class="text-green-600 hover:text-green-500"
+      href="https://code.visualstudio.com/"
+      target="_blank">VSCode</a>
+    +
+    <a
+      class="text-green-600 hover:text-green-500"
+      href="https://github.com/johnsoncodehk/volar"
+      target="_blank">Volar</a>
+  </p>
+
+  <p class="text-gray-900">
+    <a
+      class="text-green-600 hover:text-green-500"
+      href="https://vitejs.dev/guide/features.html"
+      target="_blank">Vite Documentation</a>
+    |
+    <a
+      class="text-green-600 hover:text-green-500"
+      href="https://v3.vuejs.org/"
+      target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button
+    class="mt-4 px-4 py-2 bg-green-600 text-white font-semibold rounded-md hover:bg-green-500 focus:ring-2 ring-offset-2 ring-green-600 active:bg-green-700"
+    type="button"
+    @click="count++"
+  >
+    count is: {{ count }}
+  </button>
+
+  <p class="mt-4 text-gray-900">
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
